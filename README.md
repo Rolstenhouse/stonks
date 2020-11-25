1929454aeedc52f9e413f8639ac011695cf89d81
rolstenhouse <rolsthoorn12@gmail.com>
2020-11-24 22:51:52 -0800

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Built off of create-react-app, this app should power stonks.robertolsthoorn.com
+
+Local dev: `yarn start`
+Local dev (https): `https=TRUE npm run`
+Deployment: `npm run deploy`
--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+} from "@material-ui/core";
 
 function App() {
+  useEffect(() => {
+    fetchPortfolio();
+  }, []);
+
+  const [holdings, setHoldings] = useState([]);
+  const [trades, setTrades] = useState([]);
+
+  const fetchPortfolio = () => {
+    axios.get("https://api.withlaguna.com/stonks/holdings").then((res) => {
+      setHoldings(res.data.holdings);
+    });
+    axios.get("https://api.withlaguna.com/stonks/trades").then((res) => {
+      setTrades(res.data.trades);
+    });
+  };
+
+  const toPercentage = (num) => {
+    return `${(num*100).toFixed(2)}%`;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +47,24 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Table>
+            <TableHead></TableHead>
+            <TableBody>
+              {holdings.map((holding) => {
+                // Calculate return
+                const percentageReturn = (holding.institution_price - holding.cost_basis)/holding.cost_basis;
+                const amountHeld = holding.institution_value;
+                return (
+                  <TableRow>
+                    <TableCell>{holding.ticker_symbol}</TableCell>
+                    <TableCell>{holding.name}</TableCell>
+                    <TableCell>{amountHeld.toFixed(2)}</TableCell>
+                    <TableCell>{toPercentage(percentageReturn)}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </a>
       </header>
     </div>
